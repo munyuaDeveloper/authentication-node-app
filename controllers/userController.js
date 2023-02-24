381f79eac65a83272cc59347c96aef6487c5e3bb
munyuaDeveloper <munyuapeter06@gmail.com>
2023-02-24 15:52:32 +0300

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead'
+  });
+};
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
